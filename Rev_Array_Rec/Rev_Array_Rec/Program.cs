054044b1d1ac78a655ef464cb9c3abbd1ab07eb0
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2022-02-14 21:46:13 +0530

    Add files via upload

---
+++ Rev_Array_Rec/Rev_Array_Rec/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rev_Array_Rec
+{
+    class Program
+    {
+        static void revArray(int[] arr,int s,int e)
+        {
+            int temp;
+            if (s >= e)
+                return;
+            temp = arr[s];
+            arr[s] = arr[e];
+            arr[e] = temp;
+
+            revArray(arr, s + 1, e - 1);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 6, 8, 9, 12, 23 };
+            int n = arr.Length;
+            Console.WriteLine("Old Array : ");
+            for (int i = 0; i < n; i++)
+                Console.Write(arr[i] + " ");
+            revArray(arr, 0, n - 1);
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("New Array : ");
+            for (int i = 0; i < n; i++)
+                Console.Write(arr[i] + " ");
+
+            Console.ReadLine();
+        }
+    }
+}
